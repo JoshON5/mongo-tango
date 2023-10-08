5c1ef8db93b5750d9e69e750194fc90b91c809ba
Joshua Nichols <joshua@DOOMBOT-2.local>
2023-10-07 19:05:30 -0500

    refactored the code to use async/await instead of .then() chains

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,45 +1,49 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
-    getThought(req, res) {
-        Thought.find().
-        then((thought) => {
-        if (!thought) {
-            res.status(404).json({ message: 'Nothing found :( '} )
-        }
-        res.json(thought)
-       } )
-        .catch((err) => res.status(500).json(err)) 
+	async getThought(req, res) {
+		try {
+			const thoughts = await Thought.find();
+			if (thoughts.length === 0) {
+				res.status(404).json({ message: 'Nothing found :(' });
+			} else {
+				res.json(thoughts);
+			}
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	},
 
-    },
-    getSingleThought(req, res) {
-        Thought.findOne( { _id: req.params.thoughtId } )
-        .select('-__v')
-        .then((thought) => {
-            if(!thought) {
-                res.status(404).json( { message: 'No Thought found with this Id :(' } )
-            }
-            res.json(thought)
-        }
-        )
-        .catch((err) => res.status(500).json(err))
-    },
-    createThought(req, res) {
-        Thought.create(req.body)
-        .then(({ _id }) => {
-            return User.findOneAndUpdate(
-                { _id : req.body.userId },
-                { $push: { thoughts: _id } },
-                { new: true }
-            )
-        })
-        .then((thought) => {
-            if(!thought) {
-                res.status(404)
-                .json({ message: 'No User found with this Id :( '} );
-            };
-            res.json(thought)
-        })
-        .catch((err) => res.status(500).json(err) );
-    }
-}
+	async getSingleThought(req, res) {
+		try {
+			const thought = await Thought.findOne({
+				_id: req.params.thoughtId,
+			}).select('-__v');
+			if (!thought) {
+				res.status(404).json({ message: 'No Thought found with this Id :(' });
+			} else {
+				res.json(thought);
+			}
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	},
+
+	async createThought(req, res) {
+		try {
+			const { _id } = await Thought.create(req.body);
+			const user = await User.findOneAndUpdate(
+				{ _id: req.body.userId },
+				{ $push: { thoughts: _id } },
+				{ new: true }
+			);
+			if (!user) {
+				res.status(404).json({ message: 'No User found with this Id :(' });
+			} else {
+				res.json(user);
+			}
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	},
+};
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,30 +1,40 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
-    getUser(req, res) {
-        User.find()
-        .then((user) => res.json(user))
-        .catch((err) => res.status(500).json(err));
-    },
-    createUser(req, res) {
-        User.create(req.body)
-        .then((user) => res.json(user))
-        .catch((err) => {
-            console.log(err)
-            return res.status(500).json(err)
-        });
-    },
-    getSingleUser(req, res) {
-        User.findOne( { _id: req.params.userId } )
-            .populate("thoughts")
-            .populate("friends")
-            .select("-__v")
-            .then((user => {
-                if (!user) {
-                    res.status(404).json( { message: 'No User found with this Id :( '} )
-                }
-            })
-            )
-            .catch((err) => res.status(500).json(err))
-    },
-}
+	async getUser(req, res) {
+		try {
+			const users = await User.find();
+			return res.json(users);
+		} catch (err) {
+			console.log(err);
+			return res.status(500).json(err);
+		}
+	},
+	async createUser(req, res) {
+		try {
+			const user = await Users.create(req.body);
+			res.json(user);
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	},
+	async getSingleUser(req, res) {
+		try {
+			const user = await User.findOne({ _id: req.params.userId })
+				.populate('thoughts')
+				.populate('friends')
+				.select('-__v');
+
+			if (!user) {
+				return res
+					.status(404)
+					.json({ message: 'No User found with this Id :( ' });
+			}
+
+			res.json(user);
+		} catch (err) {
+			console.log(err);
+			return res.status(500).json(err);
+		}
+	},
+};
