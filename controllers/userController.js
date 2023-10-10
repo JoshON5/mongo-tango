d307227b39a63894a9a107abb302e210cfa85040
Joshua Nichols <joshua@DOOMBOT-2.local>
2023-10-09 22:26:01 -0500

    added code to the userController functions to delete and update user. also added in the Thoughts Schema in the delete function so when the User is deleted so are the Thoughts associated with that User. Also added code in the userRoutes to implement those functions from the controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,4 +37,33 @@ module.exports = {
 			return res.status(500).json(err);
 		}
 	},
+	async updateUser(req, res) {
+		try {
+			const user = await User.findOneAndUpdate(
+				{ _id: req.params.userId },
+				{ $set: req.body },
+				{ runValidators: true, new: true }
+			);
+			if (!user) {
+				return res
+					.status(404)
+					.json({ message: 'No User found with this Id :(' });
+			}
+			res.json(user);
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	},
+	async deleteUser(req, res) {
+		try {
+			const user = await User.findOneAndDelete({ _id: req.params.userId });
+			if (!user) {
+				return res.status(404).json({ message: 'No User found with this Id :(' });
+			}
+			await Thought.deleteMany({ _id: { $in: user.thoughts } });
+			res.json({ message: 'User deleted with associated Thoughts also deleted!' });
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	},
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,12 +3,17 @@ const router = require('express').Router();
 const {
     getUser,
     getSingleUser,
-    createUser
+    createUser,
+    updateUser,
+    deleteUser
 } = require('../../controllers/userController');
 
 router.route('/').get(getUser).post(createUser);
 
 router.route('/:userId')
-.get(getSingleUser);
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser)
+
 
 module.exports = router;
